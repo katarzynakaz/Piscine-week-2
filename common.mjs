bab8e20796622198331de295d3a229307428aae5
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-10-14 16:55:15 +0100

    cleaning

--- common.mjs
+++ common.mjs
@@ -1,8 +1,5 @@
 // This is a placeholder file which shows how you can define functions which can be used from both a browser script and a node script. You can delete the contents of the file once you have understood how it works.
-import { monthSelector } from "./month-selector.mjs";
-export function getGreeting() {
-    return "Hello";
-}
+
 import { activateMonthSelector } from "./month-selector.mjs";
 import { renderCalendar } from "./render-month.mjs";
 
