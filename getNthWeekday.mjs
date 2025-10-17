5e70adf31cb978aa04097904e854e7c586c8ae6e
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-10-17 17:38:53 +0100

    updating the logic

--- getNthWeekday.mjs
+++ getNthWeekday.mjs
@@ -1,18 +1,13 @@
 export function getNthWeekday(year, month, weekday, n) {
-  const firstDay = new Date(year, month - 1, 1); 
-  const daysInMonth = new Date(year, month, 0).getDate();
-
+  const firstDay = new Date(year, month - 1, 1);
+  
   if (n > 0) {
-    
-    const firstWeekday = firstDay.getDay(); 
+    const firstWeekday = firstDay.getDay();
     const dayOffset = (weekday - firstWeekday + 7) % 7;
     const date = 1 + dayOffset + (n - 1) * 7;
-
-    if (date > daysInMonth) return null; 
     return new Date(year, month - 1, date);
   } else {
-  
-    const lastDay = new Date(year, month, 0).getDate(); 
+    const lastDay = new Date(year, month, 0).getDate();
     const lastDateObj = new Date(year, month - 1, lastDay);
     const lastWeekday = lastDateObj.getDay();
     const offset = (lastWeekday - weekday + 7) % 7;
